5552ed06bdc007dc67e9e46a1c9c0b642c74d69d
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2023-10-23 01:01:19 -0300

    creating foreign key profissao_id to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->unsignedBigInteger('profissao_id');
+            $table->foreign('profissao_id')->references('profissao_id')->on('profissao')->onDelete('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
